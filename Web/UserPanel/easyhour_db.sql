da962685256b449b77d7dc2497d7e4b64463a828
Suju <suju8811@outlook.com>
2019-11-11 00:00:24 +0800

    Add files via upload

--- Web/UserPanel/easyhour_db.sql
+++ Web/UserPanel/easyhour_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2019 at 03:44 AM
+-- Generation Time: Nov 10, 2019 at 04:54 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -40,6 +40,17 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `message` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -56,7 +67,9 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_05_03_000001_create_customer_columns', 2),
+(5, '2019_05_03_000002_create_subscriptions_table', 2);
 
 -- --------------------------------------------------------
 
@@ -73,6 +86,51 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `stripe_option`
+--
+
+CREATE TABLE `stripe_option` (
+  `id` int(11) NOT NULL,
+  `stripe_pub_key` text NOT NULL,
+  `stripe_secret_key` text NOT NULL,
+  `amount` int(11) NOT NULL,
+  `currency` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stripe_option`
+--
+
+INSERT INTO `stripe_option` (`id`, `stripe_pub_key`, `stripe_secret_key`, `amount`, `currency`) VALUES
+(1, 'pk_live_KUq6p0AyXcWGPQHcXh4i6QLk', 'sk_live_Tpwv1DrXmx2gVJpSa2grD1Aa', 1999, 'USD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriptions`
+--
+
+CREATE TABLE `subscriptions` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `started_date` timestamp NULL DEFAULT NULL,
+  `amount` int(11) NOT NULL,
+  `currency` varchar(10) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `subscriptions`
+--
+
+INSERT INTO `subscriptions` (`id`, `userid`, `started_date`, `amount`, `currency`, `created_at`, `updated_at`) VALUES
+(4, 1, '2019-11-10 05:48:11', 1999, 'USD', '2019-11-10 05:48:11', '2019-11-10 05:48:11'),
+(5, 1, '2019-11-10 07:06:56', 1999, 'USD', '2019-11-10 07:06:56', '2019-11-10 07:06:56');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -81,13 +139,13 @@ CREATE TABLE `users` (
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phonenumber` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(155) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `location` varchar(155) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `location` varchar(155) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `is_running` int(2) NOT NULL DEFAULT 0,
-  `active` tinyint(1) NOT NULL,
-  `subscription_expired` tinyint(1) NOT NULL,
-  `subscription_started_date` datetime NOT NULL,
-  `paid_amount` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 0,
+  `subscription_expired` tinyint(1) NOT NULL DEFAULT 1,
+  `subscription_started_date` timestamp NULL DEFAULT NULL,
+  `paid_amount` int(11) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -97,9 +155,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `phonenumber`, `password`, `location`, `remember_token`, `is_running`, `active`, `subscription_expired`, `subscription_started_date`, `paid_amount`, `created_at`, `updated_at`) VALUES
-(1, 'Rabbit', '7868179221', '$2y$10$/31YWQ6dOCH676/KVvUzH.vIVY9jIpnpMH7R4QcMbatRAgIKt5V..', '', 'Oil4HlePduLJhwwz20mkmTrcHjNmClpU01fgsySeOHXS8huw7w35O5MJMFjL', 0, 0, 0, '0000-00-00 00:00:00', 50, '2019-11-06 12:30:31', '2019-11-07 15:31:15'),
-(4, 'suju1', '1554224820', '$2y$10$1q0/tfGSsMJl94Pey8EnGOarpwP1ze/ElksyXdgvcfKLR1vMMp1iC', '', NULL, 0, 0, 0, '0000-00-00 00:00:00', 0, '2019-11-05 17:56:07', '2019-11-06 04:52:17'),
-(5, 'zch', '1504007648', '$2y$10$wLabZfY6cKcfSGJhQpMUvunvWXgnh3dciU5inLC6V6JwoFAmwmO1q', '', NULL, 0, 0, 0, '0000-00-00 00:00:00', 0, '2019-11-06 08:38:54', '2019-11-06 08:38:54');
+(1, 'Rabbit', '7868179221', '$2y$10$/31YWQ6dOCH676/KVvUzH.vIVY9jIpnpMH7R4QcMbatRAgIKt5V..', '', 'ol9sYQBJOaPEbotW2mz1drD9HIJPt8qYPlVdbvgdVCp7QEWM9gnCT5diucfr', 0, 1, 0, '2019-11-10 07:06:56', 3998, '2019-11-06 12:30:31', '2019-11-10 07:06:56'),
+(4, 'suju', '1554224820', '$2y$10$1q0/tfGSsMJl94Pey8EnGOarpwP1ze/ElksyXdgvcfKLR1vMMp1iC', '', NULL, 0, 1, 1, '0000-00-00 00:00:00', 0, '2019-11-05 17:56:07', '2019-11-10 07:52:30'),
+(5, 'zch', '1504007648', '$2y$10$wLabZfY6cKcfSGJhQpMUvunvWXgnh3dciU5inLC6V6JwoFAmwmO1q', '', NULL, 0, 0, 1, '0000-00-00 00:00:00', 0, '2019-11-06 08:38:54', '2019-11-06 08:38:54'),
+(9, 'test', '1234567890', '$2y$10$Q/5U62Ms6clFCSUr8Nnf0.a4mHy6wdXdLM2q9k6gAY7OinbmnkAkW', NULL, NULL, 0, 0, 1, NULL, 0, '2019-11-10 07:10:35', '2019-11-10 07:18:27');
 
 --
 -- Indexes for dumped tables
@@ -109,6 +168,12 @@ INSERT INTO `users` (`id`, `name`, `phonenumber`, `password`, `location`, `remem
 -- Indexes for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,6 +187,18 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `stripe_option`
+--
+ALTER TABLE `stripe_option`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscriptions`
+--
+ALTER TABLE `subscriptions`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -141,16 +218,34 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `stripe_option`
+--
+ALTER TABLE `stripe_option`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `subscriptions`
+--
+ALTER TABLE `subscriptions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
