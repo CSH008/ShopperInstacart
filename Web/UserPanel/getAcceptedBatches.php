770405e049b003f3462ca3566f34c5e998884326
Suju <suju8811@outlook.com>
2019-11-13 15:28:20 +0800

    Add files via upload

--- Web/UserPanel/getAcceptedBatches.php
+++ Web/UserPanel/getAcceptedBatches.php
@@ -1,9 +1,16 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+
 	if (isset($_GET['phone'])) {
 	    $phone = $_GET['phone'];
 	} else {
 	    return;
 	}
+	
+	$type = 'content';
+	if (isset($_GET['type'])) {
+	    $type = 'count';
+	} 
 	
 	//$txtUrl = "http://localhost/+1".$phone."/batches_accepted.txt";
 	$txtPath = "+1".$phone."/batches_accepted.txt";
@@ -11,6 +18,11 @@
       $content = file_get_contents($txtPath);
       $arr = explode("\n", $content);           
       $cnt = count($arr);
+      if($type == 'count')
+      {
+      	  echo $cnt;
+      	  return;
+      }
       if($cnt>4) {
       	  $start = $cnt-4;
       } else {
