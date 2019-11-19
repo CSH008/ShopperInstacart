dd96dc1442d300b577b28e81ae8dd9afbde6a5bf
Suju <suju8811@outlook.com>
2019-11-19 18:39:44 +0800

    Add files via upload

--- Web/UserPanel/getAcceptedBatches.php
+++ Web/UserPanel/getAcceptedBatches.php
@@ -20,8 +20,21 @@
       $cnt = count($arr);
       if($type == 'count')
       {
-      	  echo $cnt;
-      	  return;
+      	  //%d/%m/%y
+      	$rcnt = 0;
+      	for($i=0;$i<$cnt;$i++) {
+      	   $curdate = date('Y-m-d');
+           $datetime1 = new DateTime($curdate);            
+           $a = explode(" ", $arr[$i]);
+           $aa = explode("/", $a[0]);
+           $started_date = date($aa[2].'-'.$aa[1].'-'.$aa[0]);
+           $datetime2 = new DateTime($started_date);
+           $interval = $datetime1->diff($datetime2);
+           $days = $interval->format('%a');
+           if($days==0) $rcnt++;
+        }
+      	echo $rcnt;
+      	return;
       }
       if($cnt>4) {
       	  $start = $cnt-4;
--- Web/UserPanel/userScriptTurnOnOff.php
+++ Web/UserPanel/userScriptTurnOnOff.php
@@ -23,20 +23,16 @@
 		$editEstimate = $_GET['editEstimate'];
 		$deliveryService = $_GET['deliveryService'];			
 		
+		$string = file_get_contents($foldername.'/settings.json');
+		$response = json_decode($string, true);
+		
 		$response['PHONE'] = '+1'.$phonenumber;
 		$response['PASSWORD'] = $password;
 		$response['MINIMUM_PRICE'] = (int)$editEstimate;
 		$response['LOCATION_LATITUDE'] = $latitude;
 		$response['LOCATION_LONGITUDE'] = $longitude;
-		
-	    $response['ACCESS_TOKEN'] = "ccb4aef8bf6620cb23c6516ae1381887c73f02e845600fb2e4476e7f502e62c6";
-	    $response['CLIENT_ID'] = "2c34304d1ea8a8e56626e0ac8939eb486c08a9e40d0a113a95f76ec196097a47";
-	    $response['CLIENT_SECRET'] = "c3e1d27a5682a614ce860fb3683cb9a6c8d7fe0e519fc1dbe2622aece2ca6658";
-	    $response['KOCHAVA_DEVICE_ID'] = "KA3601571807197t2f0e32f059644f2ba020cf82441d62a4";
-	    $response['REQUEST_PAUSE_TIME'] = 0.00;
-	    $response['DEVICE_TOKEN'] = "dEufrctRID8:APA91bHg3I73QPT42Xms1IpWGGANUflVeu8sPzCxoAhJ0Z5K28xVCKwQPrZ_gNWNP2RXXHKhZo7uxHdH0n8QXWzOkDQkBUlXkt-zia_jExlYcUC6F5bVGfLdFHLOATkIYcBKq5v0ovdu";
 	   	$response['DELIVERY_SERVICE'] = $deliveryService;
-		$response['ZONE'] = 183;
+	   	$response['REQUEST_STOP'] = false;
 	} else {
 		$string = file_get_contents($foldername.'/settings.json');
 		$response = json_decode($string, true);
