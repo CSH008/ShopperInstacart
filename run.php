6ead9f39930869ae0459e7300380c95a7bf391be
Suju <suju8811@outlook.com>
2019-11-19 18:38:22 +0800

    Add files via upload

--- run.php
+++ run.php
@@ -2,8 +2,13 @@
 	header('Access-Control-Allow-Origin: *');
 	if (isset($_GET['phonenumber'])) {
 	    $phonenumber = $_GET['phonenumber'];
+	    if(isset($_GET['extension']))
+	    {
+	    	$ext = $_GET['extension'];
+	    	exec("START  ".$phonenumber.".".$ext);
+	    } else
+	    	exec("START  ".$phonenumber);
 	    echo 'success';
-	    exec("START  ".$phonenumber.".exe");	    
 	} else {
 		echo 'failed';
 	}	
