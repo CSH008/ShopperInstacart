bc0156f2a664907bd25f93cdd6b661b1f2139f22
Suju <suju8811@outlook.com>
2019-11-22 16:08:35 +0800

    Add files via upload

--- Web/UserPanel/easyhour_db.sql
+++ Web/UserPanel/easyhour_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2019 at 07:06 PM
+-- Generation Time: Nov 22, 2019 at 09:05 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -99,7 +99,7 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `sms_to_newuser`, `sms_to_allusers`, `email_to_allusers`, `created_at`, `updated_at`) VALUES
-(1, 'Welcome New User', 'Hi, All Users Ok', 'Hi, All Users Ok Ok', NULL, '2019-11-16 09:22:29');
+(1, 'Welcome New User', 'Hi, All Users Ok aaabcdefg', 'Hi, All Users Ok aaabcd', NULL, '2019-11-21 19:06:35');
 
 -- --------------------------------------------------------
 
@@ -154,7 +154,12 @@ CREATE TABLE `principles` (
 --
 
 INSERT INTO `principles` (`id`, `content`, `created_at`, `updated_at`) VALUES
-(1, 'Dashboard Content  12345678999', NULL, '2019-11-16 08:51:10');
+(1, 'Dashboard Content 1', '2019-11-16 08:51:10', '2019-11-20 02:04:53'),
+(2, 'Dashboard Content 2', '2019-11-17 08:51:10', '2019-11-17 08:51:10'),
+(3, 'Dashboard Content 3', '2019-11-18 08:51:10', '2019-11-17 08:51:10'),
+(4, 'Dashboard Content 4', '2019-11-19 08:51:10', '2019-11-16 08:51:10'),
+(13, 'Dashboard Content 5', '2019-11-20 02:01:37', '2019-11-20 02:04:42'),
+(14, 'Dashboard Content 6', '2019-11-20 02:05:10', '2019-11-20 02:05:10');
 
 -- --------------------------------------------------------
 
@@ -210,10 +215,12 @@ CREATE TABLE `users` (
   `location` varchar(155) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `ip_address` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `extension` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `is_running` int(2) NOT NULL DEFAULT 0,
   `active` tinyint(1) NOT NULL DEFAULT 0,
   `subscription_expired` tinyint(1) NOT NULL DEFAULT 1,
   `subscription_started_date` timestamp NULL DEFAULT NULL,
+  `subscription_end_date` timestamp NULL DEFAULT NULL,
   `paid_amount` int(11) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -224,11 +231,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `phonenumber`, `email`, `password`, `location`, `remember_token`, `ip_address`, `is_running`, `active`, `subscription_expired`, `subscription_started_date`, `paid_amount`, `created_at`, `updated_at`, `admin_id`) VALUES
-(1, 'Rabbit', '7868179221', NULL, '$2y$10$pASaH4ciDvJIT1haUpv4puTTfee3/SkKqA.WH4Gzx0k9SpW7oBZuO', '', 'ZYk4Apxw2ie6nbgPuoGtqSNKT5BoEFCplRofg0eLlflxQgAdziWoVcp96qq6', '52.206.230.16 ', 0, 1, 0, '2019-11-09 16:00:00', 0, '2019-11-06 12:30:31', '2019-11-16 08:28:33', 0),
-(4, 'suju', '1554224820', NULL, '$2y$10$1q0/tfGSsMJl94Pey8EnGOarpwP1ze/ElksyXdgvcfKLR1vMMp1iC', '', NULL, '52.206.230.16 ', 0, 1, 1, NULL, 0, '2019-11-05 17:56:07', '2019-11-10 07:52:30', 0),
-(5, 'zch', '1504007648', NULL, '$2y$10$wLabZfY6cKcfSGJhQpMUvunvWXgnh3dciU5inLC6V6JwoFAmwmO1q', '', NULL, '52.206.230.16 ', 0, 0, 1, NULL, 0, '2019-11-06 08:38:54', '2019-11-15 23:53:48', 0),
-(10, 'Rabbit', '4047236476', NULL, '$2y$10$jwk10cG67Q.S0sum69mxdujcTvuz4KGFGxBhjbxsVzhmrVboDcm6a', NULL, NULL, '52.206.230.16 ', 0, 1, 1, NULL, 0, '2019-11-12 15:56:17', '2019-11-12 15:56:17', 0);
+INSERT INTO `users` (`id`, `name`, `phonenumber`, `email`, `password`, `location`, `remember_token`, `ip_address`, `extension`, `is_running`, `active`, `subscription_expired`, `subscription_started_date`, `subscription_end_date`, `paid_amount`, `created_at`, `updated_at`, `admin_id`) VALUES
+(1, 'Rabbit', '7868179221', 'redhat2@gmail.com', '$2y$10$pASaH4ciDvJIT1haUpv4puTTfee3/SkKqA.WH4Gzx0k9SpW7oBZuO', '', '2q54DWqN3tFNcfX4Y3L2yqbyZwxyUXw5HBVQrsSUNAWgQ0XgbZ1CX9SgNLfC', 'localhost', 'exe', 0, 1, 1, '2019-11-09 16:00:00', '2019-11-16 16:00:00', 0, '2019-11-06 12:30:31', '2019-11-20 01:15:06', 0),
+(4, 'suju', '1554224820', 'redhat4@gmail.com', '$2y$10$1q0/tfGSsMJl94Pey8EnGOarpwP1ze/ElksyXdgvcfKLR1vMMp1iC', '', NULL, '52.206.230.16 ', NULL, 0, 1, 1, NULL, NULL, 0, '2019-11-05 17:56:07', '2019-11-10 07:52:30', 0),
+(5, 'zch', '1504007648', 'redhat3@gmail.com', '$2y$10$wLabZfY6cKcfSGJhQpMUvunvWXgnh3dciU5inLC6V6JwoFAmwmO1q', '', NULL, '52.206.230.16 ', NULL, 0, 0, 1, NULL, NULL, 0, '2019-11-06 08:38:54', '2019-11-15 23:53:48', 0),
+(10, 'Rabbit', '4047236476', 'redhat1@gmail.com', '$2y$10$jwk10cG67Q.S0sum69mxdujcTvuz4KGFGxBhjbxsVzhmrVboDcm6a', NULL, NULL, '52.206.230.16 ', NULL, 0, 1, 1, NULL, NULL, 0, '2019-11-12 15:56:17', '2019-11-12 15:56:17', 0);
 
 --
 -- Indexes for dumped tables
@@ -334,7 +341,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `principles`
 --
 ALTER TABLE `principles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `stripe_option`
